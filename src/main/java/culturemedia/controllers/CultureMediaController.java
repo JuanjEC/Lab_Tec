df099e095c168a7c76ad8a4a14332505764be641
JuanjEC <juanj.echeverry@udea.edu.co>
2024-11-03 16:13:21 -0500

    R10-Adding–controller

---
+++ src/main/java/culturemedia/controllers/CultureMediaController.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package culturemedia.controllers;
+
+import java.util.*;
+
+import culturemedia.exception.VideoNotFoundException;
+import culturemedia.model.Video;
+import culturemedia.service.impl.CultureMediaServiceImpl;
+
+public class CultureMediaController {
+
+	private final CultureMediaServiceImpl cultureMediaService;
+
+
+	public CultureMediaController(CultureMediaServiceImpl cultureMediaService) {
+		this.cultureMediaService = cultureMediaService;
+	}
+
+
+	public List<Video> find_allVideos() throws VideoNotFoundException {
+		List<Video> videos = null;
+		videos = cultureMediaService.findAll();
+		return videos;
+	}
+
+
+}
+/**
+ *
+ * @author Juan
+ */
