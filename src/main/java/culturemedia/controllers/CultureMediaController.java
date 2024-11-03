932b5d529a26fd6c4edde0021d73f5d57e7dc753
JuanjEC <juanj.echeverry@udea.edu.co>
2024-11-03 17:30:29 -0500

    R10-Adding–controller-corrected

--- src/main/java/culturemedia/controllers/CultureMediaController.java
+++ src/main/java/culturemedia/controllers/CultureMediaController.java
@@ -2,31 +2,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package culturemedia.controllers;
+package culturoteca.controllers;
 
-import java.util.*;
+import java.util.List;
+import java.util.Collections;
 
-import culturemedia.exception.VideoNotFoundException;
-import culturemedia.model.Video;
-import culturemedia.service.impl.CultureMediaServiceImpl;
+import culturoteca.exception.VideoNotFoundException;
+import culturoteca.model.Video;
+import culturoteca.service.impl.CultureMediaServiceImpl;
 
 public class CultureMediaController {
 
-	private final CultureMediaServiceImpl cultureMediaService;
+    private final CultureMediaServiceImpl cultureMediaService;
 
+    public CultureMediaController(CultureMediaServiceImpl cultureMediaService) {
+        this.cultureMediaService = cultureMediaService;
+    }
 
-	public CultureMediaController(CultureMediaServiceImpl cultureMediaService) {
-		this.cultureMediaService = cultureMediaService;
-	}
-
-
-	public List<Video> find_allVideos() throws VideoNotFoundException {
-		List<Video> videos = null;
-		videos = cultureMediaService.findAll();
-		return videos;
-	}
-
-
+    public List<Video> findAllVideos() {
+        try {
+            return cultureMediaService.findAll();
+        } catch (VideoNotFoundException e) {
+            return Collections.emptyList();
+        }
+    }
 }
 /**
  *
